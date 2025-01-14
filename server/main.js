01c1b4caeeea1a06bb00de0461c9c29369e5f5a9
Antonio Servin <ing.antonio.servin@gmail.com>
2025-01-13 22:16:31 -0600

    add error and disconnect

--- server/main.js
+++ server/main.js
@@ -11,6 +11,17 @@ let server_url = `http://localhost:8030/api/`
 
 io.on("connection", async (socket) => {
     console.log("Nuevo cliente conectado: ", socket.id)
+
+    // Add error handling
+    socket.on("error", (error) => {
+        console.error("Socket error:", error);
+    });
+    
+    // Add disconnect handling
+    socket.on("disconnect", (reason) => {
+        console.log("Cliente desconectado:", socket.id, "Razón:", reason);
+    });
+    
     socket.emit("connection-socket", { socket_id: socket.id, success: true })
 
     socket.on("send-form", async (data) => {
